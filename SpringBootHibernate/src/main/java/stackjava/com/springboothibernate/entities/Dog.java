e194b48572b550b8f7a40dea16d2ff1eaeb02af6
datlanhnhat <datlm@infoplusvn.com>
2020-06-02 22:51:05 +0700

    add lombok

--- SpringBootHibernate/src/main/java/stackjava/com/springboothibernate/entities/Dog.java
+++ SpringBootHibernate/src/main/java/stackjava/com/springboothibernate/entities/Dog.java
@@ -1,5 +1,10 @@
 package stackjava.com.springboothibernate.entities;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Dog {
     private Long id;
     private String name;
